32e94a265c9a0822741ff0b61f9471d1b6b37a44
JonasThers <jonasthers1992@gmail.com>
2023-06-03 11:38:40 +0300

    Add woocommerce support

--- functions.php
+++ functions.php
@@ -8,7 +8,12 @@ function remove_admin_login_header() {
     remove_action('wp_head', '_admin_bar_bump_cb');
 }
 
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
 add_action('wp_enqueue_scripts', 'idaslet_register_styles');
 add_action('get_header', 'remove_admin_login_header');
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 ?>
