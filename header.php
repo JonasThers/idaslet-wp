e48f76b7e3c7cefd4c79ed948d8e68c7d914b823
JonasThers <jonasthers1992@gmail.com>
2023-06-03 10:40:20 +0300

    Make title dynamic

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
   <body>
     <header class="header">
-      <h1 class="header__title">Idaslet</h1>
+      <h1 class="header__title"><?php bloginfo('name'); ?></h1>
     </header>
