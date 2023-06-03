ef554ad8449b0360e82e86081c7e40fdb83bb5d2
JonasThers <jonasthers1992@gmail.com>
2023-06-03 09:41:01 +0300

    Create header and footer files

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+        <h2 class="footer__header">Follow Idaslet on social media</h2>
+        <Socials />
+        <div class="footer__emails">
+          <p>Booking: <a href="mailto:Booking@Idaslet.dk">Booking@Idaslet.dk</a></p>
+          <p>General inquiries: <a href="mailto:Idaslet@Idaslet.dk">Idaslet@Idaslet.dk</a></p>
+        </div>
+      </footer>
+  </body>
+</html>
