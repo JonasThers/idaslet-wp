3a0306beb7404c1cc357b9c2b3a326c94f466ba3
JonasThers <jonasthers1992@gmail.com>
2023-06-03 16:21:53 +0300

    Only have header scroll effetc on front page

--- header.php
+++ header.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
-  </head>
-  <body>
-    <header id="header" class="header">
-      <h1 id="header-title" class="header__title"><?php bloginfo('name'); ?></h1>
-    </header>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>
+    <?php bloginfo('name'); ?>
+  </title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header id="<?php echo (is_front_page()) ? "header" : ''; ?>"
+    class="header <?php echo (is_front_page()) ? "" : 'header--black'; ?>">
+    <h1 id="<?php echo (is_front_page()) ? "header-title" : ''; ?>"
+      class="header__title <?php echo (is_front_page()) ? "" : 'header__title--white'; ?>"><?php bloginfo('name'); ?>
+    </h1>
+  </header>
