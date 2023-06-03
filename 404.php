101849c7a91f0ef162080d0ecdf67a2f817df7f3
JonasThers <jonasthers1992@gmail.com>
2023-06-03 11:03:50 +0300

    Add 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="content center">
+    <h2>404</h2>
+    <h3>Wow there, dear viking! Where are you going?</h3>
+    <h3>Not here, that's for sure!</h3>
+    <h3>Try going to the <a href="<?php echo home_url(); ?>">front page</a></h3>
+</div>
+
+<?php get_footer(); ?>
