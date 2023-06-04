a6f3f88e6b81db52113a12c36b4d7dd9d52ee3ef
JonasThers <jonasthers1992@gmail.com>
2023-06-04 16:50:18 +0300

    Override single product page

--- functions.php
+++ functions.php
@@ -17,9 +17,17 @@ function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 
+function my_remove_product_tabs( $tabs ) {
+    unset( $tabs['additional_information'] );
+    unset( $tabs['reviews'] );
+    return $tabs;
+}
+
 add_action('wp_enqueue_scripts', 'idaslet_register_styles');
 add_action('wp_enqueue_scripts', 'idaslet_register_scripts');
 add_action('get_header', 'remove_admin_login_header');
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_filter( 'woocommerce_product_tabs', 'my_remove_product_tabs', 98 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
 ?>
