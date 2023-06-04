8078f3b420f1c3df23162ff15afdafc5270c8225
JonasThers <jonasthers1992@gmail.com>
2023-06-04 16:50:55 +0300

    More changes to cart

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -20,6 +20,7 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_before_cart' ); ?>
 
 <div class="container">
+	<h2 class="cart-title">Your cart</h2>
     <form class="woocommerce-cart-form cart-override" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
 
