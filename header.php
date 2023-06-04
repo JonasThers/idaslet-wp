4a0291afc76f0053aaac4d94294b46f47489fed1
JonasThers <jonasthers1992@gmail.com>
2023-06-04 18:41:19 +0300

    Add minicart

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
   <?php wp_head(); ?>
 </head>
 
+<?php global $woocommerce; ?>
+
 <body>
   <header id="<?php echo (is_front_page()) ? "header" : ''; ?>"
     class="header <?php echo (is_front_page()) ? "" : 'header--black'; ?>">
@@ -19,4 +21,5 @@
         class="header__title <?php echo (is_front_page()) ? "" : 'header__title--white'; ?>"><?php bloginfo('name'); ?>
       </h1>
     </a>
+    <a href="/cart" class="minicart"><img class="minicart__logo" src="<?= get_template_directory_uri() . './assets/img/shopping-cart.png'; ?>" /><?php if ($woocommerce->cart->get_cart_contents_count()) : ?><span class="minicart__amount"><?php echo $woocommerce->cart->get_cart_contents_count() ?></span><?php endif; ?></a>
   </header>
