3c37822dc9b932f16b09165b02d89223b357867e
JonasThers <jonasthers1992@gmail.com>
2023-08-12 16:10:23 +0300

    Update to Woocommerce 7.9.0

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -20,11 +20,11 @@ defined( 'ABSPATH' ) || exit;
 /*
  * @hooked wc_empty_cart_message - 10
  */
-echo "<h2>Your cart is empty</h2>";
+do_action( 'woocommerce_cart_is_empty' );
 
 if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 	<p class="return-to-shop">
-		<a class="button back-button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo home_url(); ?>">
+		<a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
 			<?php
 				/**
 				 * Filter "Return To Shop" text.
@@ -32,7 +32,7 @@ if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 				 * @since 4.6.0
 				 * @param string $default_text Default text.
 				 */
-				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to front page', 'woocommerce' ) ) );
+				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
 			?>
 		</a>
 	</p>
