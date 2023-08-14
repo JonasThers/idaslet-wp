da377d2e941cf1d81c67e4c9a4b579f55768fe71
JonasThers <jonasthers1992@gmail.com>
2023-08-14 20:07:51 +0300

    Product page QA

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -42,14 +42,7 @@ do_action('woocommerce_before_main_content');
     <?php
     global $post;
 
-    $heading = apply_filters('woocommerce_product_description_heading', __('Description', 'woocommerce'));
     ?>
-
-    <?php if ($heading): ?>
-        <h2>
-            <?php echo esc_html($heading); ?>
-        </h2>
-    <?php endif; ?>
 
     <?php the_content(); ?>
 </div>
